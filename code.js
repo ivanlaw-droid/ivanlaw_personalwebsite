a189fc96b9828e38711d256f6f598e0c87b6d35c
ivanlaw-droid <ivanlaw020814@gmail.com>
2025-10-03 18:01:25 +0800

    (3)10-3

--- code.js
+++ code.js
@@ -1,10 +1,23 @@
-    var title = document.queryselector("h1");
-    title.innerHTML = "This is the title from Javascript!";
+    //var title = document.queryselector("h1");
+    //title.innerHTML = "This is the title from Javascript!";
 
-    var button = document.querySelector("#press me");
+    var button = document.querySelector("#CV");
 
     button.addEventListener("click",myfunction);
 
     function myfunction(){
       alert("Let me tell you more about me!")
     }
+
+    var mynode = document.createElement("div");
+    mynode.id = "work1_intro";
+    mynode.innerHTML = "The work is an exhibition";
+    mynode.style.color = "blue";    
+
+    mynode.addEventListener("click",welcomeToWork1);
+    document.querySelector("#my_work1").appendChild(mynode);
+
+function welcomeToWork1(){
+    mynode.innerHTML = "Thank you for your interest in my work!"
+}
+
