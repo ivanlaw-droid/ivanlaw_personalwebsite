251782f4fa01378728a111befebd23e9b3180421
ivanlaw-droid <ivanlaw020814@gmail.com>
2025-11-07 12:00:19 +0800

    fix

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ const width = 90, height = 500;
 const svg = d3.select("#map");
 const projection = d3.geoMercator()
  .scale(140)
- .translate({width /2, height / 1.4});
+ .translate([width / 2, height / 1.4]);
 
  const path = d3.geoPath().projection(projection);
 
