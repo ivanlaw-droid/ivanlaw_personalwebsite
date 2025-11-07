0d8d2f9a40f798645678e6bdf873fc0834a8c1c7
ivanlaw-droid <ivanlaw020814@gmail.com>
2025-11-07 11:59:24 +0800

    try

---
+++ map.js
@@ -0,0 +1,10 @@
+const width = 90, height = 500;
+
+const svg = d3.select("#map");
+const projection = d3.geoMercator()
+ .scale(140)
+ .translate({width /2, height / 1.4});
+
+ const path = d3.geoPath().projection(projection);
+
+ 
